c64d1e687677a9cffc38b044b70296fd5bc5883a
Thomas Klebel <t.klebel@hotmail.com>
2018-01-09 18:19:26 +0100

    add helper to access sample data

---
+++ R/example.R
@@ -0,0 +1,19 @@
+#' Get path to jstor example
+#' 
+#' jstor includes several sample files for demonstration purposes. This helper
+#' makes them easy to access.
+#' 
+#' The code for this function was adapted from the package `readr`.
+#' 
+#' @param path Name of the example file. If `NULL`, the example files will be listed.
+#' @export
+#' @examples
+#' jstor_example()
+#' jstor_example("sample_with_references.xml") 
+jstor_example <- function(path = NULL) {
+  if (is.null(path)) {
+    dir(system.file("extdata", package = "jstor"))
+  } else {
+    system.file("extdata", path, package = "jstor", mustWork = TRUE)
+  }
+}
