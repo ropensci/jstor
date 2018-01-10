fdd156514a48ddd64b510d6f68bb408bdc4e37fe
Thomas Klebel <t.klebel@hotmail.com>
2018-01-10 16:06:21 +0100

    add helper to construct class for testing

--- R/helpers.R
+++ R/helpers.R
@@ -8,3 +8,10 @@ extract_basename <- function(file_path, type) {
   basename(file_path) %>%
     stringr::str_extract(paste0(".*?(?=\\.", type, ")"))
 }
+
+
+as_jstor <- function(x) {
+  stopifnot(is.data.frame(x))
+  
+  structure(x, class = c("jstor", "data.frame"))
+}
