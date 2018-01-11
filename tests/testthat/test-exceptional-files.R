79f40cf00c54db03f2c1327d9d67de3aff7ef6f0
Thomas Klebel <t.klebel@hotmail.com>
2018-01-11 14:35:26 +0100

    test flagging articles

--- tests/testthat/test-exceptional-files.R
+++ tests/testthat/test-exceptional-files.R
@@ -1,19 +1,19 @@
-# library(xml2)
-# library(magrittr)
-# context("exceptional-files")
-# 
-# 
-# # import files
-# result <- "testfiles/test-file-single-author.xml" %>%
-#   find_metadata() %>%
-#   flag_special_articles()
-# 
-# result_erratum <- "testfiles/test-file-erratum.xml" %>%
-#   find_metadata() %>%
-#   flag_special_articles()
-# 
-# 
-# test_that("articles are flagged correctly", {
-#   expect_identical(result[["special_article"]], "none")
-#   expect_identical(result_erratum[["special_article"]], "erratum")
-# })
+library(xml2)
+library(magrittr)
+context("exceptional-files")
+
+
+# import files
+result <- "testfiles/standard_case.xml" %>%
+  find_metadata() %>%
+  flag_special_articles()
+
+result_erratum <- "testfiles/erratum.xml" %>%
+  find_metadata() %>%
+  flag_special_articles()
+
+
+test_that("articles are flagged correctly", {
+  expect_identical(result[["special_article"]], "none")
+  expect_identical(result_erratum[["special_article"]], "erratum")
+})
