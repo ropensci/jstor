6e01435a24ed59fa693105ba756d565d02eb8778
Thomas Klebel <t.klebel@hotmail.com>
2018-01-15 17:04:51 +0100

    update tests

--- tests/testthat/test-books.R
+++ tests/testthat/test-books.R
@@ -6,6 +6,9 @@ library(magrittr)
 result <- "testfiles/standard_book.xml" %>%
   find_book()
 
+alternative <- "testfiles/book-alternative-case.xml" %>% 
+  find_book()
+
 # tests -----
 test_that("Input data is checked", {
   expect_error(find_book("my_path.txt"))
@@ -22,7 +25,7 @@ test_that("book-meta fields are correct", {
   expect_identical(result[["book_id"]], "j.ctt24hdz7")
   expect_identical(result[["discipline"]], "Political Science")
   expect_identical(
-    find_book("testfiles/multiple-disciplines.xml")[["discipline"]],
+    find_book("testfiles/book-alternative-case.xml")[["discipline"]],
     "Political Science; Sociology"
   )
   expect_identical(result[["book_title"]], "The 2006 Military Takeover in Fiji")
@@ -31,6 +34,9 @@ test_that("book-meta fields are correct", {
   expect_identical(result[["pub_month"]], 4L)
   expect_identical(result[["pub_year"]], 2009L)
   expect_identical(result[["isbn"]], "9781921536502; 9781921536519")
+  expect_identical(alternative[["isbn"]], NA_character_)
   expect_identical(result[["publisher_name"]], "ANU E Press")
   expect_identical(result[["publisher_location"]], "Canberra")
+  expect_identical(alternative[["n_pages"]], 271L)
+  expect_identical(result[["n_pages"]], NA_integer_)
 })
