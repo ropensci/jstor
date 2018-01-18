8f7c4b1184140b454cf0b5f5c5fd7cfc57bfb633
Thomas Klebel <t.klebel@hotmail.com>
2018-01-18 12:10:05 +0100

    add top-level documentation
    closes #17

---
+++ R/jstor_package.R
@@ -0,0 +1,20 @@
+#' jstor: Read Data from JSTOR/DfR
+#' 
+#' The tool [Data for Research (DfR)](http://www.jstor.org/dfr/) by JSTOR is a
+#' valuable source for citation analysis and text mining. `jstor`
+#' provides functions and suggests workflows for importing
+#' datasets from DfR.
+#' 
+#' Please refer to the vignettes for information on how to use the package:
+#' 
+#' `browseVignettes("jstor")`
+#' 
+#' If you encounter any issues or have ideas for new features, please file an
+#' issue at [https://github.com/tklebel/jstor/issues].
+#' 
+#' @author Thomas Klebel
+#' 
+#' @name jstor
+#' @docType package
+#' 
+NULL
