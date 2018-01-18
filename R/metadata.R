5b85b914840a13653b09423b6a8b2da8c940ffe4
Thomas Klebel <t.klebel@hotmail.com>
2018-01-18 15:22:15 +0100

    expand documentation for metadata #2

--- R/metadata.R
+++ R/metadata.R
@@ -2,15 +2,29 @@
 #'
 #' `find_metadata()` extracts meta-data from JSTOR-XML files.
 #'
-#' **Publication dates**:
+#' @param file_path A `.xml`-file for a journal-article.
 #'
-#' Always the first entry is being extracted, which should correspond to the oldest
-#' date, in case there is more than one date.
+#' @return A `data.frame` containing the extracted meta-data with the following
+#' columns:
+#' - journal_id *(chr)*: The jcode or a DOI. If both are present, the jcode
+#'  (=publisher-id) is extracted.
+#' - basename_id *(chr)*: The filename of the original .xml-file. Can be used 
+#'   for joining with full-texts.
+#' - article_id *(chr)*: The article id, either a combination of digits 
+#'   (`pub-id-type="jstor"`) or a DOI (`pub-id-type="doi"`).
+#' - article_title *(chr)*: The title of the article.
+#' - volume *(chr)*: The volume the article was published in.
+#' - issue *(chr)*: The issue the article was published in.
+#' - language *(chr)*: The language of the article.
+#' - pub_day *(int)*: Publication day, if specified.
+#' - pub_month *(int)*: Publication month, if specified.
+#' - pub_yea *(int)*: Year of publication.
+#' - first_pag *(int)*: Page number for the first page of the article.
+#' - last_page *(int)*: Page number for the last page of the article.
 #'
-#' @param file_path A file from JSTOR containing meta-data which can be read in with
-#' [xml2::read_xml()]. `find_metadata` takes care of importing the file.
-#'
-#' @return A `tibble` containing the extracted meta-data.
+#' A note about publication dates: always the first entry is being extracted,
+#' which should correspond to the oldest date, in case there is more than one
+#' date.
 #'
 #' @export
 #' @examples 
