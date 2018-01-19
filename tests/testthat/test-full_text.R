73a1df405b794799990fd9768e9aa49c24d20298
Thomas Klebel <t.klebel@hotmail.com>
2018-01-19 12:25:45 +0100

    add tests for full-text

--- tests/testthat/test-full_text.R
+++ tests/testthat/test-full_text.R
@@ -1,17 +1,34 @@
-# context("full_text")
-# 
-# # base path for id ------
-# crazy_path <- "../Entpackt//soc_a-j/jpalestud/2012/41/2/10.1525%2Fjps.2012.xli.issue-2/10.1525%2Fjps.2012.xli.2.204/10.1525%2Fjps.2012.xli.2.204.txt"
-# standard_path <- "../Entpackt//soc_k-z/sozialewelt/1952/3/4/i40039236/40874627/40874627.txt"
-# 
-# test_that("getting base name of file works", {
-#   expect_identical(extract_basename(crazy_path, type = "txt"), "10.1525%2Fjps.2012.xli.2.204")
-#   expect_identical(extract_basename(standard_path, type = "txt"), "40874627")
-# })
-# 
-# # encoding -----
-# file_path <- "testfiles/test-file-full-file.txt"
-# 
-# test_that("guessing encoding works", {
-#   expect_identical(get_encoding(file_path), "UTF-8")
-# })
+context("full_text")
+
+# base path for id ------
+crazy_path <- "../Entpackt//soc_a-j/jpalestud/2012/41/2/10.1525%2Fjps.2012.xli.issue-2/10.1525%2Fjps.2012.xli.2.204/10.1525%2Fjps.2012.xli.2.204.txt"
+standard_path <- "../Entpackt//soc_k-z/sozialewelt/1952/3/4/i40039236/40874627/40874627.txt"
+
+test_that("getting base name of file works", {
+  expect_identical(extract_basename(crazy_path, type = "txt"), "10.1525%2Fjps.2012.xli.2.204")
+  expect_identical(extract_basename(standard_path, type = "txt"), "40874627")
+})
+
+# encoding -----
+file_path <- "testfiles/full-text.txt"
+
+test_that("guessing encoding works", {
+  expect_identical(get_encoding(file_path), "ASCII")
+})
+
+# text ------
+lorem <- '<plain_text> <page sequence="1">Lorem ipsum dolor sit amet, 
+consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore 
+magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in 
+voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim
+id est laborum.
+</page>  </plain_text>\n'
+
+
+test_that("full-text is read correctly", {
+  text <- read_full_text(file_path)
+  
+  expect_identical(lorem, text[["full_text"]])
+})
