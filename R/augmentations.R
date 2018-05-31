85f51f58a72c55c1b5a0f8b118dbd5a63653b5b5
Thomas Klebel <t.klebel@hotmail.com>
2018-05-31 16:34:02 +0200

    document new functions

--- R/augmentations.R
+++ R/augmentations.R
@@ -1,3 +1,22 @@
+#' Clean data from DfR
+#' 
+#' This function takes data from [jst_get_article()] or [jst_get_book()] and
+#' applies helper functions for cleaning the data.
+#' 
+#' Data from DfR is inherently messy. For many examples see 
+#' `vignette("known-quirks", package = "jstor")`. `jst_augment()` is a
+#' convenience function that tries to deal with a few common tasks to
+#' clean the data.
+#' 
+#' For journal articles, it calles [jst_clean_page()] for first and last page,
+#' [jst_unify_journal_id()] and [jst_add_total_pages()].
+#' 
+#' @param meta_data Data which was processed via [jst_get_article()] or 
+#' [jst_get_book()]
+#' 
+#' @return A cleaned tibble.
+#' @seealso [jst_clean_page()] [jst_unify_journal_id()] [jst_add_total_pages()]
+#' 
 #' @export
 jst_augment <- function(meta_data) {
   col_names <- names(meta_data)
@@ -19,7 +38,23 @@ jst_augment <- function(meta_data) {
   }
 }
 
+#' Clean a character vector of pages
+#' 
+#' This function tries to convert character vectors into integers. This function
+#' should not be called on page ranges.
+#' 
+#' @param page A character vector for pages.
 #' @export
+#' @examples 
+#' jst_clean_page("2")
+#' 
+#' # anything that is not a digit gets removed
+#' jst_clean_page("A2-")
+#' 
+#' # a weird format from the American Journal of Sociology is convered correctly
+#' jst_clean_page("AJSv104p126")
+#' # this is done by searching for "p", and if it is found, extracting the
+#' content after "p".
 jst_clean_page <- function(page) {
   # check if any has weird format like with AJS: AJSv104p126
   is_complicated <- str_detect(page, "p")
@@ -33,6 +68,13 @@ jst_clean_page <- function(page) {
   convert_page(page)
 }
 
+#' Add total count of pages
+#' 
+#' This function adds a column with the total count of pages. It calls 
+#' [jst_get_total_pages()] which does the work.
+#' 
+#' @param meta_data Data which was processed via [jst_get_article()].
+#' @param page_col The name of the new column with total count of pages.
 #' @export
 jst_add_total_pages <- function(meta_data, page_col = n_pages) {
   page_col <- rlang::enquo(page_col)
@@ -48,12 +90,22 @@ jst_add_total_pages <- function(meta_data, page_col = n_pages) {
 #' This function is a simple helper to calculate the total number of pages of
 #' an article.
 #' 
-#' This function deals with three cases:
+#' This function deals with four cases:
 #' 
+#' - if all three arguments are missing, NA is returned.
 #' - if page_range is supplied, the number of pages is calculated from it.
-#' - if only the first page is supplied, NA is returned
+#' - if only the first page is supplied, NA is returned.
 #' - if first and last page are supplied, the number of pages is calculated as
-#' `last_page - first_page + 1`
+#' `last_page - first_page + 1`.
+#' 
+#' The algorithm to parse page ranges works as follows: A typical page range is
+#' `1-10, 200` where the article starts at page 1, ends at page 10, and has an
+#' erratum at page 200. For this case, the range is calculated as
+#' `range + single_page`, as in`(10 - 1 + 1) + 1 = 11`. Sometimes multiple 
+#' ranges are given: `1-10, 11-20`. For those cases all ranges are summed:
+#' `(10 - 1 + 1) + (20 - 11 + 1) = 20`. Another specification for multiple 
+#' ranges is `1-10+11-20`, which is treated similarly.
+#' 
 #' 
 #' @param first_page The first page of an article (numeric).
 #' @param last_page The last page of an article (numeric).
@@ -66,18 +118,29 @@ jst_add_total_pages <- function(meta_data, page_col = n_pages) {
 #' # calculate pages from first and last page
 #' first_pages <- sample(30:50, 10)
 #' last_pages <- first_pages + sample(5:20, 10)
+#' page_ranges <- rep(NA_character_, 10)
 #' 
-#' jst_get_total_pages(first_pages, last_pages)
+#' jst_get_total_pages(first_pages, last_pages, page_ranges)
 #'
 #' # get pages from page range
-#' jst_get_total_pages(page_range = "51 - 70, 350")
+#' jst_get_total_pages(NA_real_, NA_real_, "51 - 70")
+#' jst_get_total_pages(NA_real_, NA_real_, "51 - 70, 350")
+#' jst_get_total_pages(NA_real_, NA_real_, "350, 51 - 70")
+#' jst_get_total_pages(NA_real_, NA_real_, "51 - 70, 80-100")
+#' jst_get_total_pages(NA_real_, NA_real_, "51-70+350")
 jst_get_total_pages <- function(first_page, last_page, page_range) {
+  if (!(is.numeric(first_page) && is.numeric(last_page))) 
+    abort("`first_page` and `last_page` must be numeric.")
+  
+  if (!is.character(page_range))
+    abort("`page_range` must be a character vecttor.")
+  
   ll <- list(first_page, last_page, page_range)
   if (!identical(length(unique(lengths(ll))), 1L)) {
     abort("All inputs must have the same length.")
   }
   
-  case_when(
+  dplyr::case_when(
     is.na(first_page) & is.na(last_page) & is.na(page_range) ~ NA_real_,
     !is.na(page_range) ~ parse_ranges(page_range),
     is.na(page_range) & !is.na(first_page) & is.na(last_page) ~ NA_real_,
@@ -90,13 +153,38 @@ jst_get_total_pages <- function(first_page, last_page, page_range) {
 
 
 
-
+#' Unify journal IDs
+#' 
+#' This function is a simple wrapper to unify journal ids.
+#' 
+#' Date on journal ids can be found in three columns:
+#' `journal_pub_id`, `journal_jcode` and `journal_doi`. From my experience,
+#' most of the time the relevant dat ais present in `journal_pub_id` or
+#' `journal_jcode`, with `journal_jcode` being to most common identifier.
+#' This function takes the value from `journal_pub_id`, and if it is missing,
+#' that from `journal_jcode`. `journal_doi` is currently disregarded
+#' 
+#' @param meta_data Data which was processed via [jst_get_article()].
+#' @param new_col The name for the column with unified id.
+#' @param remove_cols Should the original columns be removed after unifiyng?
+#' 
+#' @return A modified `tibble`.
+#' 
+#' @return A modified tibble.
 #' @export
-jst_unify_journal_id <- function(meta_data) {
-  meta_data %>%
-    dplyr::mutate(journal_id = case_when(is.na(journal_pub_id) ~ journal_jcode,
-                                         TRUE ~ journal_pub_id)) %>%
-    dplyr::select(-journal_pub_id, -journal_jcode, -journal_doi)
+jst_unify_journal_id <- function(meta_data, new_col = journal_id, 
+                                 remove_cols = TRUE) {
+  out <- meta_data %>%
+    dplyr::mutate(!!new_col := dplyr::case_when(
+      is.na(journal_pub_id) ~ journal_jcode,
+      TRUE ~ journal_pub_id
+    ))
+  
+  if (remove_cols) {
+    out <- dplyr::select(out, -journal_pub_id, -journal_jcode, -journal_doi)
+  } 
+  
+  out
 }
 
 
