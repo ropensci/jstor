5f52cdf9bb3490adbf330b8828dab829540707c1
Thomas Klebel <t.klebel@hotmail.com>
2018-09-27 22:36:48 +0200

    note that date in documentation should be updated

--- data-raw/get_jstor_journals.R
+++ data-raw/get_jstor_journals.R
@@ -4,3 +4,5 @@ library(jstor)
 # if jst_get_journal_overview works correctly, the data can be updated via
 jstor_journals <- jst_get_journal_overview(most_recent = T)
 usethis::use_data(jstor_journals, internal = T, overwrite = T)
+
+# update date in documentation
