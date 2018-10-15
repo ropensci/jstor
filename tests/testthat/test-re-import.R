d357c4de439fd6f6944b32b15bc3f083c53e3b2a
Thomas Klebel <t.klebel@hotmail.com>
2018-10-15 18:08:36 +0200

    add line break

--- tests/testthat/test-re-import.R
+++ tests/testthat/test-re-import.R
@@ -281,7 +281,8 @@ test_that("files are removed", {
 
 
 test_that("only .csv files can be reimported", {
-  expect_error(jst_re_import(jst_example("book.xml")), "Only .csv-files which were generated")
+  expect_error(jst_re_import(jst_example("book.xml")), 
+               "Only .csv-files which were generated")
 })
 
 
